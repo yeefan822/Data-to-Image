d12509ea49c91adc2b268342ea5def42a47d1d7a
yeefan822 <68040785+yeefan822@users.noreply.github.com>
2022-09-20 21:43:43 +1200

    Add input dirty image

--- fti.py
+++ fti.py
@@ -1,9 +1,21 @@
 from PIL import Image
 import numpy as np
+from csv import reader
 
-slice56 = np.random.random((1024, 1024))
-
+with open('dirty.csv', 'r') as read_obj:
+    # pass the file object to reader() to get the reader object
+    csv_reader = reader(read_obj)
+    array=[]
+    # Iterate over each row in the csv using reader object
+    for row in csv_reader:
+        # row variable is a list that represents a row in csv
+        array.append(row)
+# slice56 = np.random.random((1024, 1024))
+# print(array)
+formatted = np.asarray(array)
+floatversion = formatted.astype(np.float64)
 # convert values to 0 - 65535 int16 format
-formatted = (slice56 * 65535 / np.max(slice56)).astype('uint16')
-img = Image.fromarray(formatted)
+# formatted = (slice56 * 65535 / np.max(slice56)).astype('uint16')
+intversion = floatversion.astype(np.int16)
+img = Image.fromarray(intversion)
 img.show()
