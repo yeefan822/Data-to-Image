b785348c2a529382fbcd7ccceedc3f88a035632f
yeefan822 <68040785+yeefan822@users.noreply.github.com>
2022-09-20 20:52:19 +1200

    Create 1024*1024 with uint16

---
+++ fti.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import numpy as np
+
+slice56 = np.random.random((1024, 1024))
+
+# convert values to 0 - 255 int8 format
+formatted = (slice56 * 65535 / np.max(slice56)).astype('uint16')
+img = Image.fromarray(formatted)
+img.show()
