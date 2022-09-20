673a42bf783a46ccb889a876b645a205b41df26a
yeefan822 <68040785+yeefan822@users.noreply.github.com>
2022-09-20 21:22:18 +1200

    Update comment

--- fti.py
+++ fti.py
@@ -3,7 +3,7 @@ import numpy as np
 
 slice56 = np.random.random((1024, 1024))
 
-# convert values to 0 - 255 int8 format
+# convert values to 0 - 65535 int16 format
 formatted = (slice56 * 65535 / np.max(slice56)).astype('uint16')
 img = Image.fromarray(formatted)
 img.show()
