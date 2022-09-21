e03a8b28869ffb69d92c0849a65ebba059e7f2a6
yeefan822 <68040785+yeefan822@users.noreply.github.com>
2022-09-21 21:23:26 +1200

    Smooth the way of generating images using rgb

--- fti.py
+++ fti.py
@@ -2,20 +2,22 @@ from PIL import Image
 import numpy as np
 from csv import reader
 
-with open('dirty.csv', 'r') as read_obj:
+with open('residual_image_1024.csv', 'r') as read_obj:
     # pass the file object to reader() to get the reader object
     csv_reader = reader(read_obj)
     array=[]
     # Iterate over each row in the csv using reader object
     for row in csv_reader:
         # row variable is a list that represents a row in csv
-        array.append(row)
+        temp = row[0].split()
+        array.append(np.asarray(temp))
 # slice56 = np.random.random((1024, 1024))
 # print(array)
 formatted = np.asarray(array)
 floatversion = formatted.astype(np.float64)
-# convert values to 0 - 65535 int16 format
+
+
 # formatted = (slice56 * 65535 / np.max(slice56)).astype('uint16')
-intversion = floatversion.astype(np.int16)
+intversion = floatversion.astype(np.int32)
 img = Image.fromarray(intversion)
 img.show()
